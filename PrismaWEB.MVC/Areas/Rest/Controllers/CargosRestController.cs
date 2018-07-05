5ab317210c53009283a09d43b0812cda2f16b4d9
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2018-07-05 19:28:43 -0300

    Adicionado receptor de chamados rest

---
+++ PrismaWEB.MVC/Areas/Rest/Controllers/CargosRestController.cs
@@ -0,0 +1,35 @@
+﻿using ProjetoModeloDDD.Application.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoModeloDDD.MVC.Areas.Rest.Controllers
+{
+    public class CargosRestController : Controller
+    {
+        private readonly ICargoAppService _cargoApp;
+
+        public CargosRestController(ICargoAppService cargoApp)
+        {
+            _cargoApp = cargoApp;
+        }
+
+        // GET: Rest/CargosRest
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public JsonResult GetAll()
+        {
+            return Json(_cargoApp.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetById(int id)
+        {
+            return Json(_cargoApp.GetById(id), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
